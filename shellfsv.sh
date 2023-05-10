6188a8aa57a296fdbdaee4e34632b0300cf278f3
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2023-05-10 20:59:19 +0200

    Define mains APIso

--- shellfsv.sh
+++ shellfsv.sh
@@ -4,6 +4,24 @@ source def.sh
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]
     then
         echo "usage: shellfsv [--help] [-h]"
+        echo "                [--lireconfig] [-lv]"
+        echo "                [--lirecartecps] [-lcps]"
+        echo "                [--liredroitsvitale] [-lsv]"
         echo "Invokes Olaqin CloudFSV API"
         echo "Implitly uses defs in def.sh"
+fi
+
+if [ "$1" = "--lireconfig" ] || [ "$1" = "-lc" ]
+    then
+        echo $API_LIRE_CONFIG
+fi
+
+if [ "$1" = "--lirecarteps" ] || [ "$1" = "-lcps" ]
+    then
+        echo $API_LIRE_CARTE_PS
+fi
+
+if [ "$1" = "--liredroitsvitale" ] || [ "$1" = "-lsv" ]
+    then
+        echo $API_LIRE_DROITS_VITALE
 fi
