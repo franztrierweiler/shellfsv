4a3a90fa1904b83f50595cfb37bade01ab8c7bd5
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2023-05-11 13:13:17 +0200

    Added loop

--- readvitale.sh
+++ readvitale.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 source def.sh
 
-echo "$TARGET"
+echo "--> Tests $TARGET"
+
+i=""
+c=0
+while [ "$i" != "0" ]
+do 
+    echo "$c"
+    echo "$i"
+    ./shellfsv.sh -lc >"file_$c"
+    ./shellfsv.sh -lc -turbo >"file_turbo_$c"
+    read -p "Next>" i
+    ((c=c+1))
+done
+exit 0
