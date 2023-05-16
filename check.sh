d9e5bed442050e516d5742e1b7ea53409507b8a4
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2023-05-16 10:01:58 +0200

    Add additional controls over results.

--- check.sh
+++ check.sh
@@ -5,12 +5,31 @@ file_list=`ls out/`
 
 for file in $file_list
 do
-    if [ "`echo "$file" | wc -c`" = 36 ]
+    echo "$file" | wc -c
+    if [ `echo "$file" | wc -c` = 30 ]
+    then
+        target=`echo "$file" | sed 's/<file_>/& turbo_/'`
+        #echo "Target = $target"
+        diff -q "out/$file" "out/$target" >> result_not_identical_files
+        diff -s "out/$file" "out/$target" >> result_identical_files
+
+        cat "out/$file" | grep "{\"codeResult\":0,\"dataResult\"" >> result_no_error
+        cat "out/$target" | grep "{\"codeResult\":0,\"dataResult\"" >> result_no_error_turbo
+
+        error=`cat "out/$file" | jq '.codeResult'`
+        echo "error=$error"
+        if (( error != 0 ))
         then
-            #echo "$file"
-            target=`echo "$file" | sed 's/<file_>/& turbo_/'`
-            #echo "Target = $target"
-            diff -q "out/$file" "out/$target" >> result_not_identical_files
-            diff -s "out/$file" "out/$target" >> result_identical_files
+            banner 1
+            cat "out/$file" >> result_error
+        fi
+
+        error=`cat "out/$target" | jq '.codeResult'`
+        echo "error=$error"
+        if (( $error != 0 ))
+        then
+            banner 2
+            cat "out/$target" >> result_error_turbo
+        fi
     fi
 done
