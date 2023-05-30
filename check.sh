c9ba6c928f57866c4865a60d7af42c8e738e3a6c
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2023-05-30 21:10:13 +0200

    Bug fixes

--- check.sh
+++ check.sh
@@ -3,15 +3,16 @@
 nb_files=`ls out/ | wc -l`
 file_list=`ls out/`
 
+i=0
 for file in $file_list
 do
-    echo "$file" | wc -c
     if [ `echo "$file" | wc -c` = 30 ]
     then
-        target=`echo "$file" | sed 's/<file_>/& turbo_/'`
-        #echo "Target = $target"
+        # Fucking gnu sed !
+        target=`echo "$file" | sed 's/file_/&turbo_/'`
+        echo "File = $file"q
+        echo "Target = $target"
         diff -q "out/$file" "out/$target" >> result_not_identical_files
-        diff -s "out/$file" "out/$target" >> result_identical_files
 
         cat "out/$file" | grep "{\"codeResult\":0,\"dataResult\"" >> result_no_error
         cat "out/$target" | grep "{\"codeResult\":0,\"dataResult\"" >> result_no_error_turbo
@@ -31,5 +32,8 @@ do
             banner 2
             cat "out/$target" >> result_error_turbo
         fi
+        ((i=i+1))
     fi
 done
+echo "$i fichiers analysés"
+echo "`cat result_not_identical_files | wc -l` non identiques"
